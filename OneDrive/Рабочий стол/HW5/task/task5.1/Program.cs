561e0b1d186598022ddb747cb207ec0b23f139fd
julia B <yuliy-7@yandex.ru>
2023-02-18 13:38:17 +0300

    HW5.1

---
+++ OneDrive/Рабочий стол/HW5/task/task5.1/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+int[] CreateArray(int len = 5)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]};");
+    }
+
+}
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+int[] array = CreateArray();
+ShowArray(array);
+Console.WriteLine($"количество чётных чисел в массиве: {CountEven(array)}");
+
+
+
