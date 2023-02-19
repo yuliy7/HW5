60cba0e763da1dd990cec9eb14145356455c2b92
julia B <yuliy-7@yandex.ru>
2023-02-19 14:58:26 +0300

    HW6.2

---
+++ OneDrive/Рабочий стол/HW6/task/task6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double Prompt(string massage)
+{
+    System.Console.Write($"{massage}: ");
+    double result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+(double, double) Point (double b1, double b2, double k1, double k2)
+{
+    double x = (b2-b1)/(k1-k2);
+    double y = k1 * x + b1;
+    return (x,y);
+}
+double b1 = Prompt("Введите число b1: ");
+double b2 = Prompt("Введите число b2: ");
+double k1 = Prompt("Введите число k1: ");
+double k2 = Prompt("Введите число k2: ");
+(double x, double y) = Point(b1, b2, k1, k2);
+Console.Write($"Прямые пересекаются в точке: ({x},{y})");
+
