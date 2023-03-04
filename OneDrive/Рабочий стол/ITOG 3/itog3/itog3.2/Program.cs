b30734df4db566e3559b8447953a919db45593cf
julia B <yuliy-7@yandex.ru>
2023-03-04 13:49:04 +0300

    itog3.2

---
+++ OneDrive/Рабочий стол/ITOG 3/itog3/itog3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Рекурсия!
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int SumNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return;
+    }
+   return SumNumbers(numberM + 1)+numberM;
+    
+}
+System.Console.Write(SumNumbers(1,15));
