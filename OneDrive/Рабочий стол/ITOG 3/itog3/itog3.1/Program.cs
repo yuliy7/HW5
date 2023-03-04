17cf020dd34116ff3821ff68bbb300df28738a2e
julia B <yuliy-7@yandex.ru>
2023-03-04 13:48:48 +0300

    itog3.1

---
+++ OneDrive/Рабочий стол/ITOG 3/itog3/itog3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8" */
+
+void ShowNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return;
+    }
+    if (numberM % 2 == 0)
+    {
+        return;
+    }
+    System.Console.Write(numberM + ", ");
+    ShowNumbers(numberM + 1, numberN);
+}
+ShowNumbers(5, 10);
