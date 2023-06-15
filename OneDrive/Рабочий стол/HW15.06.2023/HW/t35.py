612f85fbc02181386625c2d66c0420ea702b5668
julia B <yuliy-7@yandex.ru>
2023-06-15 19:22:36 +0300

    Задача 37

--- OneDrive/Рабочий стол/HW15.06.2023/HW/t35.py
+++ OneDrive/Рабочий стол/HW15.06.2023/HW/t35.py
@@ -6,12 +6,12 @@
 #Input: 5
 #Output: yes
 
-def Chek(number):
-     for i in range(2,number):
-         if number%i==0:
-             return(f"Число {number} не простое")
+# def Chek(number):
+#      for i in range(2,number):
+#          if number%i==0:
+#              return(f"Число {number} не простое")
     
-         return(f"Число простое")    
-number=int(input("Ввeдите число: "))
-res=Chek(number)
-print(res)  
+#          return(f"Число простое")    
+# number=int(input("Ввeдите число: "))
+# res=Chek(number)
+# print(res)  
